1214b3b458933a039b6cd54aff61fc26a8efc776
niko <asdasd>
2021-05-04 21:59:19 -0500

    50 states app

--- client/src/components/StateMap.vue
+++ client/src/components/StateMap.vue
@@ -5,8 +5,8 @@
         <p v-if="state.visited">You have visited this state</p>
         <p v-else>You have not yet visited this state</p>
 
-        <div id="map-container">
-            <l-map v-bind:center="mapCenter" v-bind:zoom="state.zoom">
+        <div id="map-container" v-if="dataReady">
+            <l-map ref="map" v-on:ready="onMapReady" v-bind:center="mapCenter" v-bind:zoom="state.zoom">
                 <l-tile-layer 
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; OpenStreetMap contributors">
@@ -28,7 +28,9 @@ export default {
     },
     data() {
         return {
-            state: {}
+            state: {},
+            dataReady: false, //used to track if data is ready, start as false, when api call is complete, change to true
+            mapReady: false //used to track if map is ready, when map is loaded, set to true
         }
     },
     mounted() {
@@ -38,8 +40,32 @@ export default {
     methods: {
         fetchStateData() {
             this.$stateService.getOneState(this.state.name).then( state => {
-                this.state = state 
+                this.state = state
+                this.dataReady = true //we know data is fetched by this point so can change property to true
+            }).catch( err => {
+                //404
+                if (err.response && err.response.status === 404) {
+                    this.state.name = '?' // todo think aout a better way to communicate to the user
+                } else {
+                    //500 server error
+                    alert('Sorry, error fetching data about this state') //msg for user
+                    console.error(err) //for the developer
+                }
             })
+        },
+        onMapReady() {
+            this.mapReady = true //tied as v-on in l-map element in map-container div
+        },
+        setMapView() {
+            if (this.mapReady && this.dataReady) {
+                //todo make sure map shows correct part of world and zoom level
+                this.$refs.map.leafletObject.setView(this.mapCenter, this.zoom) //html element with ref map
+            }
+        }
+    },
+    computed: {
+        mapCenter() {
+            return [ this.state.lat, this.state.lon ]
         }
     }
 }
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 let express = require('express')
 let states_api = require('./routes/states')
+let path = require('path') //path library
 
 let app = express()
+
+//create path variable - joining location of code plus the client and dist directories
+//makes path/location on computer that runs the code. where vue app is
+let vueAppPath = path.join(__dirname, 'client', 'dist') 
+app.use(express.static(vueAppPath)) //requests to the application will return the serve the files in this directory (index.html) - how site will be visible
 
 app.use(express.json())
 
